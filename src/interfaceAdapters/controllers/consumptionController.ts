cee163a78c8d49dd23305f2c3d52f0da7fe0ac60
Agatha Menezes <agatha.menezes@wisereducacao.com>
2024-09-20 11:39:18 -0300

    feat: revieve body data and read hidrometer value

--- src/interfaceAdapters/controllers/consumptionController.ts
+++ src/interfaceAdapters/controllers/consumptionController.ts
@@ -5,15 +5,18 @@ import { geminiGateway, googleApiFileGateway } from '../../app';
 import { Upload } from '../../application/useCases/Upload';
 
 async function upload(req: Request, res: Response) {
-  const file = req.file;
-  if (!file) {
+  const image = req.file;
+
+  const { customer_code, measure_datetime, measure_type } = req.body;
+
+  if (!image) {
     res.sendStatus(400);
     return;
   }
 
   const uploadUseCase = new Upload(googleApiFileGateway);
-  const fileResponse = await uploadUseCase.upload(file.path, {
-    mimeType: file.mimetype,
+  const fileResponse = await uploadUseCase.upload(image.path, {
+    mimeType: image.mimetype,
     displayName: 'Jetpack drawing',
   });
 
@@ -25,10 +28,12 @@ async function upload(req: Request, res: Response) {
         fileUri: fileResponse.file.uri,
       },
     },
-    { text: 'Describe how this product might be manufactured.' },
+    {
+      text: 'Quantos metros cubicos o hidrometro está marcando? Responda utilizando json no seguinte formato: { valor: %s }',
+    },
   ]);
 
-  if (result) res.status(201).json(result);
+  if (result) res.status(201).json(JSON.parse(result));
   else res.sendStatus(400);
 }
 
--- src/interfaceAdapters/routes/consumptionRoutes.ts
+++ src/interfaceAdapters/routes/consumptionRoutes.ts
@@ -1,8 +1,26 @@
 import { Router } from 'express';
+import multer from 'multer';
+import path from 'path';
+
 import consumptionController from '../controllers/consumptionController';
+
+// Configurando armazenamento de arquivos no multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Diretório de upload
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname),
+    );
+  },
+});
+const upload = multer({ storage: storage });
 
 const router = Router();
 
-router.post('/upload', consumptionController.upload);
+router.post('/upload', upload.single('file'), consumptionController.upload);
 
 export default router;
