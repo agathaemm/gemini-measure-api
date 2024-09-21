d68bb29345c8e11ea4d7229f78adc478a8520f1b
Agatha Menezes <agatha.menezes@wisereducacao.com>
2024-09-21 15:04:37 -0300

    refactor: remove multer

--- src/interfaceAdapters/routes/consumptionRoutes.ts
+++ src/interfaceAdapters/routes/consumptionRoutes.ts
@@ -1,25 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
-import path from 'path';
 
 import consumptionController from '../controllers/consumptionController';
 import { consumptionValidator } from '../middlewares/consumptionValidator';
 import { validationMiddleware } from '../middlewares/validationMiddleware';
-
-// Configurando armazenamento de arquivos no multer
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Diretório de upload
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname),
-    );
-  },
-});
-const upload = multer({ storage: storage });
 
 const router = Router();
 
@@ -27,7 +10,6 @@ router.post(
   '/upload',
   consumptionValidator,
   validationMiddleware,
-  upload.single('file'),
   consumptionController.upload,
 );
 
